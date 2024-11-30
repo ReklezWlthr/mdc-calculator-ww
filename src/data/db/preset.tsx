0ff6a9d7c0e0758a2b2ac6f3714b5df08c8d76d1
GunNapat <napat.wat@innovasive.co.th>
2024-11-30 21:30:34 +0700

    update: add new sonata preset

--- src/data/db/preset.tsx
+++ src/data/db/preset.tsx
@@ -53,6 +53,28 @@ export const ModifierPresets = [
     ),
   },
   {
+    name: Sonata.COORD,
+    order: 1,
+    type: 'Sonata',
+    property: [{ stat: Stats.P_ATK, value: 20, flat: false }],
+    icon: (
+      <div className="flex items-center justify-center w-11 h-11">
+        <img src={SonataIcons[Sonata.COORD]} className="w-10 h-10" />
+      </div>
+    ),
+  },
+  {
+    name: Sonata.HAVOC_2,
+    order: 1,
+    type: 'Sonata',
+    property: [{ stat: Stats.HAVOC_DMG, value: 15, flat: false }],
+    icon: (
+      <div className="flex items-center justify-center w-11 h-11">
+        <img src={SonataIcons[Sonata.HAVOC_2]} className="w-10 h-10" />
+      </div>
+    ),
+  },
+  {
     name: 'Jinzhou Skewers',
     order: 2,
     type: 'Dish',
